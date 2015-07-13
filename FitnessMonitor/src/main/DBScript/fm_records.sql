d3cd74c999c0fac067ad8201a21ac9beefdea158
michael0521 <yxm1014@gmail.com>
2015-07-13 17:44:09 -0500

    The column 'Claories' type should be int, not varchar

--- FitnessMonitor/src/main/DBScript/fm_records.sql
+++ FitnessMonitor/src/main/DBScript/fm_records.sql
@@ -2,7 +2,7 @@ CREATE TABLE fm_records
 (
    `Id`          int(10),
    `MemId`       int(10),
-   `Calories`    varchar(233),
+   `Calories`    int(233),
    `Date`        date,
    `TimeStamp`   timestamp DEFAULT 'CURRENT_TIMESTAMP'
 )
